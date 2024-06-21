e38eeafc1d0e3a9d212ff2548ca8ac223a127568
itou-syunya-0826 <146901442+itou-syunya-0826@users.noreply.github.com>
2024-06-21 16:00:42 +0900

    色々修正

--- main.cpp
+++ main.cpp
@@ -5,41 +5,35 @@
 
 int main() {
 
-	Animal* cat[3];
-	Animal* doc[3];
-	Animal* fox[3];
+	Animal* animal[3];
 
 	for (int i = 0; i < 3; i++) {
-		cat[i] = new Cat;
-		doc[i] = new Doc;
-		fox[i] = new Fox;
+		if (i == 0) {
+			animal[i] = new Cat;
+		}
+		else if (i == 1) {
+			animal[i] = new Doc;
+		}
+		else {
+			animal[i] = new Fox;
+		}
 	}
 
 	for (int i = 0; i < 3; i++) {
 		if (i == 0) {
 			printf("”L‚Ì–Â‚«º\n");
 		}
-		cat[i]->Voice();
-		if (i == 2) {
-			for (int j = 0; j < 3; j++) {
-				if (j == 0) {
-					printf("\nŒ¢‚Ì–Â‚«º\n");
-				}
-				doc[j]->Voice();
-				if (j == 2) {
-					for (int k = 0; k < 3; k++) {
-						if (k == 0) {
-							printf("\nŒÏ‚Ì–Â‚«º\n");
-						}
-						fox[k]->Voice();
-					}
-				}
-			}
+		else if (i == 1) {
+			printf("\nŒ¢‚Ì–Â‚«º\n");
 		}
+		else {
+			printf("\nŒÏ‚Ì–Â‚«º\n");
+		}
+		animal[i]->Voice();
 	}
 
 	for (int i = 0; i < 3; i++) {
-		delete cat[i], doc[i], fox[i];
+		delete animal[i];
 	}
 
 	return 0;
